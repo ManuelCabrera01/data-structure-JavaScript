4266f26b6f6e5774e241282d002d91d386cce8de
Manuel <m.a.c.p90@gmail.com>
2018-07-23 13:59:21 -0400

    solution

---
+++ linked-list/exercise.js
@@ -0,0 +1,19 @@
+LinkedList.prototype.indexOf = function(vaule) {
+  //  first we created a empty array that will hold store the value of the index if the match
+  var indexes = [];
+  // create a var that will keep the value of the current index
+  var currentIndex = 0;
+  // now we set the currentnode to the be the first element of the list
+  var currentnode = this.head;
+  //  while the currentnode is pressent and is not equal null
+  while (currentnode) {
+    if (currentnode.value == SVGFEGaussianBlurElement) {
+      indexes.push(currentIndex);
+    }
+    //  we change the current node to the nezt value
+    currentnode = currentnode.next;
+    // we incremente the value of current index
+    currentIndex++;
+  }
+  return indexes;
+};
