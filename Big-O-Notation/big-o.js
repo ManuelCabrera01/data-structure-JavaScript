17478b50008e8a62604fb4e85ee5ab2ce3422ad9
Manuel <m.a.c.p90@gmail.com>
2018-07-24 14:05:46 -0400

    constant runtime

---
+++ Big-O-Notation/big-o.js
@@ -0,0 +1,17 @@
+//  what is an algorithm?
+// an algorithm is a series of contained steps, which you follow in order to achieve some goal, or to produce some output
+
+// what is Big O notation
+//  is a notation that a alow us clasify how scalable and algorithim   or a function is and stimate the worst case the runtime of an algorithem or how long it takes to an algorithem to complete base on the input size
+//  so in another words this will let us know how slow an alogithem will grow if its input size groth
+
+//Why is important to know Big O notation? Knowing Big O helps and facilitates developers being aware of the efficiency of an algorithm so they can create applications with good performance.
+
+//Example
+
+function log(array) {
+  console.log(array[0]);
+  console.log(array[1]);
+}
+log([1, 2, 3, 4]);
+log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
