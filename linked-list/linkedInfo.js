33058b1eafb5cb5929e1a4875213983952561bd7
Manuel <m.a.c.p90@gmail.com>
2018-07-24 20:46:04 -0400

    info about likedlist structure

---
+++ linked-list/linkedInfo.js
@@ -0,0 +1,9 @@
+// perfonmance and time complecxity
+
+// it has a run time of constant runtime  :O (1); for removing adding to the head or the tail this is beacpuse we are using head and tail pointer to keep track of nodes therfore we dont have to iterates trhoug the entire list to find the head or tail
+
+// it has a linear time complexity or O (n) to search
+
+// practical use for a Linked List
+// are online gaming
+// or poker, board games dominoes
