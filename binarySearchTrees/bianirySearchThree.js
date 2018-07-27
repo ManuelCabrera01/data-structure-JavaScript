8b1eed0882ceab1de4498041a5e77c0c561a509b
Manuel <m.a.c.p90@gmail.com>
2018-07-27 18:48:15 -0400

    factorial call stack and recursion

---
+++ binarySearchTrees/bianirySearchThree.js
@@ -0,0 +1,5 @@
+// ehat is a binary search three and what is it use for
+// is a collection of node that are conected in sertain way. eahc node will have up to two child modes the left node and the right node and each node will contain some data or a value   similar to a linked list
+//  the way our binary search  work is all of a left node will be les or equal value to the parent node and all of the right node will be grater value than the parent node
+
+//  ways to tavaling throug the three  are Depht First Rtraversal Method and breadth First Traversal method
---
+++ binarySearchTrees/recursion.js
@@ -0,0 +1,32 @@
+//  what is recursion?
+// recursion is when a funtion call it self
+
+
+function func(){
+if(/*base case*/){
+return somthing
+}else{//recursive case
+
+    func()
+}
+
+
+};
+
+// example 
+// factorial of a number is that number times every number from itself down to one
+
+// 4! = 4 * 3 * 2 * 1 = 24
+ function factorial(num){
+     //
+     if(num === 1){
+        return num  
+     }else{
+         return num *factorial(num-1);
+
+     }
+
+ };
+factorial(4)
+//  callStack
+// it represent what order are function  been call  in and what variable they been call with
