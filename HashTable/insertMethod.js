d428ece156290f456ff1163d5d4829371781db3d
Manuel <m.a.c.p90@gmail.com>
2018-09-04 15:36:03 -0400

    missing refactoring

---
+++ HashTable/insertMethod.js
@@ -0,0 +1,15 @@
+// this methdo will take a key value pair turn it into a node and inserted into the correct bucket
+HashTable.prototype.insert = function(key, value) {
+  // first we have to know withc bucket our hash node is going to be store and we use the this hash method to figure it out
+  var index = this.hash(key);
+  // then we have to si if the index are empty or not
+  if (!this.buckets[index]) {
+    this.buckets[index] = new HashNode(key, value);
+  } else {
+    var currentNode = this.buckets[index];
+    while (currentNode.next) {
+      currentNode.next;
+    }
+    currentNode.next = new HashNode(key, value);
+  }
+};
